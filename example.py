084aa11d137145b90a47c7d0a42f47c0795479a0
Larry Rennoldson <92909352+1azza@users.noreply.github.com>
2022-03-08 22:39:03 +0000

    k

--- example.py
+++ example.py
@@ -4,4 +4,4 @@ import Seneca
 token = Seneca.token.Refresh('AIzaSyDXmCdeFZFJbQOtl6xupkxZw-lIOKuJQKg')
 user = Seneca.User(token)
 
-Seneca.answers.Fetch('https://app.senecalearning.com/classroom/course/9b389a80-1cb0-11e8-ba7f-85cc3dd82400/section/f2113da0-1cb3-11e8-ba7f-85cc3dd82400/session')
+Seneca.answers.Fetch('https://app.senecalearning.com/classroom/course/f4a9de91-3dcc-4e19-b180-1286357dded5/section/2d349e50-8362-4aba-b189-6f376c86b577/session')

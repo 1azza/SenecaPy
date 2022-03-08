f1f07379798997ab61d970c45be6f4bcecf497d5
Larry Rennoldson <92909352+1azza@users.noreply.github.com>
2022-03-08 17:51:55 +0000

    Slight rewrite of initialising the module

--- Seneca/__init__.py
+++ Seneca/__init__.py
@@ -6,10 +6,5 @@ from .Modules.info import Info
 from .Modules.answers import Answers
 
 token = Token()
-if token.Refresh(refresh_key) == 0:
-    print('Enter new token in Seneca\Modules\Data\data.py')
-
 locations = Locations() 
-user = User(token.idToken)
-info =Info(token.idToken)
-answers = Answers(token.idToken)
+answers = Answers()
--- example.py
+++ example.py
@@ -1,3 +1,7 @@
 import Seneca
-info = Seneca.user.GetInfo()
-Seneca.answers.Get('https://app.senecalearning.com/classroom/course/155ff5e0-374d-11e8-b1d7-1d16c592d2f8/section/053ff430-38ba-11e8-a6d2-7f9af227008a/session')
+
+token = Seneca.token.Refresh('AIzaSyDXmCdeFZFJbQOtl6xupkxZw-lIOKuJQKg')
+user = Seneca.User(token)
+print(f'Found User:\n{user.displayName}')
+
+

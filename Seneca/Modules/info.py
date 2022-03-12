7e298776a7001e8629deb40b8fe42ebd298c6406
Larry Rennoldson <92909352+1azza@users.noreply.github.com>
2022-03-12 17:04:16 +0000

    Merge info.py and user.py

--- Seneca/Modules/info.py
+++ Seneca/Modules/info.py
@@ -1,13 +1,44 @@
-from lib2to3.pgen2 import token
 import requests
-from .Data.data import refresh_key ,GLOBALHEADERS
 class Info:
-    def __init__(self, idToken):
+    def __init__(self, idToken, refresh_key): 
+        """_summary_
+
+        Args:
+            idToken (str) : The user JWT created from token.refresh
+        """        
+        self.refresh_key = refresh_key
         self.idToken = idToken
+        self.GetInfo()
 
- 
+    def GetInfo(self):
+        """Sends a GET request to the server for the user information
 
-import requests
+        Returns:
+            json: The user Json object
+        """        
+        url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/getAccountInfo"
+        querystring = {"key":self.refresh_key} #We need this key otherwise we cant acsess the account information
+        payload = {"idToken": self.idToken} #This token specifies the account we are getting the information from
+        headers = {
+            "authority": "www.googleapis.com"
+        }
+        #Sending Request to get the user information
+        response = requests.request("POST", url, json=payload, headers=headers, params=querystring)
+
+
+        #Parsing Response
+        userData = response.json().get('users')[0]
+        self.displayName = userData.get('displayName')
+        self.email = userData.get('email')
+        self.lastRefreshAt = userData.get('lastRefreshAt')
+        self.passwordHash = userData.get('UkVEQUNURUQ=')
+        self.photoUrl = userData.get('photoUrl')
+        self.createdAt = userData.get('createdAt')
+
+
+
+        return userData
+
 
 
 

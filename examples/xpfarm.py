0b1f695fda40c1341fc42818fb8ebed60b99d24c
Larry Rennoldson <92909352+1azza@users.noreply.github.com>
2022-03-27 22:19:17 +0100

    Move

--- examples/xpfarm.py
+++ examples/xpfarm.py
@@ -1,17 +1,22 @@
 import seneca
 import threading
+import concurrent.futures
 import time
-EMAIL = input('Please enter your email for senecalearning.com: ')
-PASSWORD = input('Please enter your password for senecalearning.com: ')
+import logging
+# EMAIL = input('Please enter your email for senecalearning.com: ')
+# PASSWORD = input('Please enter your password for senecalearning.com: ')
+logging.basicConfig(level=logging.INFO)
+
 def main():
     args = round(int(input('How many times would you like to complete the section? '))/4)
     
     def go(iterations):
         for i in range(iterations):
             s.SubmitData()
-    print('ok')
-    user = seneca.User(EMAIL, PASSWORD)
+    user = seneca.User('017437@brgsmail.org.uk', 'Larry1102')
     s = user.Session('g60fcc5d-57d8-4833-96b1-bae36baaf12e', user)
+    oks = user.info.getKnowledgeScore()   
+    oks = round(oks/1000000000000, 1)
     s.Start()
     
     x1 = threading.Thread(target=go, args=(args,))
@@ -30,10 +35,11 @@ def main():
     x4.join()
     
     time.sleep(1)
-    print('\n\nNew knowledge score:')    
-    ks = user.info.getKnowledgeScore()   
-    ks = round(ks/1000000000000, 1)
-    print(ks, 'T\n\n')
+    s.End()
+    nks = user.info.getKnowledgeScore()   
+    nks = round(nks/1000000000000, 1)
+    print('\n\nknowledge score increase:')    
+    print(f'{oks}T ---> {nks}T\n\n')
     main()
 main()
     

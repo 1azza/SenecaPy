11ec101d601c519f7b80f27659d1b752b2180dfb
Trypn <102833950+Trypn@users.noreply.github.com>
2022-04-02 09:35:02 +0100

    Added higher thread count support
    Added user input for thread amount wanted to be used

--- examples/xpfarm.py
+++ examples/xpfarm.py
@@ -4,7 +4,7 @@ import concurrent.futures
 import time
 import logging
 logging.basicConfig(level=logging.INFO)
-def main():                 
+def main():
     EMAIL = input('Please enter your email for senecalearning.com: ')
     PASSWORD = input('Please enter your password for senecalearning.com: ')
     try: 
@@ -15,32 +15,28 @@ def main():
             main()
 
     def sesh():
-        args = round(int(input('How many times would you like to complete the section? '))/4)
+        args = int(input('How many times would you like to complete the section? '))
+        logging.warning("High thread amounts may affect stability") 
+        threads = int(input("How many threads?"))
+        args = round(args/threads)
         def go(iterations):
             for i in range(iterations):
                 s.SubmitData()
         s = user.Session('g60fcc5d-57d8-4833-96b1-bae36baaf12e', user)
-        oks = user.info.getKnowledgeScore()   
+        oks = user.info.getKnowledgeScore()
         oks = round(oks/1000000000000, 1)
         s.Start()
-        x1 = threading.Thread(target=go, args=(args,))
-        x3 = threading.Thread(target=go, args=(args,))
-        x2 = threading.Thread(target=go, args=(args,))
-        x4 = threading.Thread(target=go, args=(args,))
-        x1.start()
-        x2.start()
-        x3.start()
-        x4.start()
-        x1.join()
-        x2.join()
-        x3.join()
-        x4.join()
+        for i in range(threads):
+            x1 = threading.Thread(target=go, args=(args,))
+            x1.start()
+        for i in range(threads):    
+            x1.join()
         time.sleep(1)
         s.End()
-        nks = user.info.getKnowledgeScore()   
+        nks = user.info.getKnowledgeScore()
         nks = round(nks/1000000000000, 1)
-        print('\n\nknowledge score increase:')    
+        print('\n\nknowledge score increase:')
         print(f'{oks}T ---> {nks}T\n\n')
         sesh()
-    sesh()  
+    sesh()
 main()

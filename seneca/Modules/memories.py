c16b6a91bd8fa20de5e85df1ebc722161e382de4
Larry Rennoldson <92909352+1azza@users.noreply.github.com>
2022-04-06 13:52:59 +0100

    Docstring

--- seneca/Modules/memories.py
+++ seneca/Modules/memories.py
@@ -4,6 +4,11 @@ import requests
 
 
 class Memories:
+    '''
+    This class is used to store the data from the memory app.
+    :param idToken: The token used to access the data.
+    :return: None
+    '''
     def __init__(self, idToken):
         self.idToken = idToken
         url = "https://memory.app.senecalearning.com/api/locations"
@@ -13,18 +18,36 @@ class Memories:
         self.data = json.loads(response.text).get('items')
 
     def __ParseInfo(self, i):
-            loc = []
-            loc.append(i.get('countryName'))
-            loc.append(i.get('name'))
-            loc.append(i.get('id'))
-            return loc
+        '''
+        Parses the information from the json file and returns a list of the required information.
+        :param i: The information from the json file.
+        :return: A list of the required information.
+        '''
+        loc = []
+        loc.append(i.get('countryName'))
+        loc.append(i.get('name'))
+        loc.append(i.get('id'))
+        return loc
 
     def AllCountries(self):
+        '''
+        This function returns a list of all the countries in the dataset.
+        :return: A list of all the countries in the dataset.
+        '''
         Countries = []
         for i in self.data:
             Countries.append((self.__ParseInfo(i)))
         return Countries
     def SetMemory(self, courseId, sectionId, locationId, courseName ,sectionTitle):
+        '''
+        Sets a memory in the Seneca Learning Memory API.
+        :param courseId: The course ID of the course.
+        :param sectionId: The section ID of the section.
+        :param locationId: The location ID of the location.
+        :param courseName: The name of the course.
+        :param sectionTitle: The title of the section.
+        :return: None
+        '''
         url = "https://memory.app.senecalearning.com/api/memories"
 
         payload = {
@@ -52,14 +75,11 @@ class Memories:
         
 
     def GetLocation(self, name:str):
-        """
-        Finds Longitude and Latitude coords from the name of the location from seneca
-        Args:
-            name (string): The name of the place to find
-
-        Returns:
-            Coords (list): Longitude and Latidud of the location
-        """                
+        '''
+        Gets the location of a location.
+        :param name: The name of the location.
+        :return: The latitude and longitude of the location.
+        '''
         for i in self.data:
             if i.get('name') == name:
                 print('Found Location for:')

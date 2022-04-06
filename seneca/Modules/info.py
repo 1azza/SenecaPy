58d7884dcf62351a5fa0579f6efc1158c323d8fa
Larry Rennoldson <92909352+1azza@users.noreply.github.com>
2022-04-06 13:59:59 +0100

    Docstring

--- seneca/Modules/info.py
+++ seneca/Modules/info.py
@@ -2,6 +2,10 @@ from ast import Subscript
 import requests
 from seneca.Modules.key import  APIKEY, CORRELATOINID
 class Info:
+    '''
+    This is a constructor for the class Info.
+    :param idToken: The access key for the API.
+    '''
     def __init__(self, idToken): 
         self.idToken = idToken
         self.headers =  {
@@ -11,13 +15,17 @@ class Info:
     def classInfo(self):
         pass
     def getAccountInfo(self):
+        '''
+        Sends a GET request to the server for the user information.
+        :return: The user Json object.
+        '''
         """Sends a GET request to the server for the user information
 
         Returns:
             json: The user Json object
         """        
         url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/getAccountInfo"
-        querystring = {"key": APIKEY} #We need this key otherwise we cant acsess the account information
+        querystring = {"key": APIKEY} #We need this key otherwise we cant access the account information
         payload = {"idToken": self.idToken} #This token specifies the account we are getting the information from
 
         #Sending Request to get the user information
@@ -35,17 +43,30 @@ class Info:
         return AccountInfo
     
     def _me(self, url):
+        '''
+        This function is used to make a GET request to the API.
+        :param url: The URL of the API.
+        :return: The response of the GET request.
+        '''
         self.headers['correlationid'] = '1647206128958::50f6180dec98105c2f1317155089ce1d'
         response = requests.request("GET", url, headers=self.headers)
         data = response.json()
         return data
     
     def getSchoolData(self):
+        '''
+        This function gets the school data from the seneca app.
+        :return: A dictionary of the school data.
+        '''
         url = 'https://schools.app.senecalearning.com/api/school-info/me'
         SchoolInfo =  self._me(url)
         return SchoolInfo
     
     def getUserData(self):
+        '''
+        This function gets the user info from the API.
+        :return: A dictionary of the user info.
+        '''
         url = 'https://user-info.app.senecalearning.com/api/user-info/me'
         UserInfo = self._me(url)
         return UserInfo
@@ -53,10 +74,18 @@ class Info:
     
     
     def getSubscriptionData(self):
+        '''
+        This function gets the subscription data from the subscription API.
+        :return: A dictionary of the subscription data.
+        '''
         url = 'https://subscriptions.app.senecalearning.com/api/subscriptions/me'
         SubscriptionData =  self._me(url)
         return SubscriptionData
     def getKnowledgeScore(self):
+        '''
+        This function returns the KnowledgeScore of the user.
+        :return: KnowledgeScore of the user.
+        '''
         url = 'https://stats.app.senecalearning.com/api/stats/users'
         data =   self._me(url)
         sessionModulesStudied = data.get('sessionModulesStudied')
